83e3838ea6cf7c7926b3b75fccc7fc11823c1625
Yanisa Hemprachitchai <aiya.yanisa@gmail.com>
2017-05-23 22:36:17 +0700

    Fixed index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-export default class checkaround extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+import {AppRegistry} from 'react-native';
+import Login from './src/scenes/Login';
+import List from './src/scenes/List';
+import Message from './src/scenes/Message';
+import Checkin from './src/scenes/Checkin';
+import Notify from './src/scenes/Notify';
+import { TabNavigator } from 'react-navigation';
+
+const checkaround = TabNavigator({
+  List: { screen: List },
+  Message: { screen: Message },
+  Checkin: { screen: Checkin },
+  Notify: { screen: Notify },
+  Home: { screen: Login }
+},{
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+
 
 AppRegistry.registerComponent('checkaround', () => checkaround);
