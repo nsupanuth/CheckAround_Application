7168a6b56457f480203aaf64833a133ab6736713
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2017-05-24 00:01:13 +0700

    Add checkin scene

--- src/scenes/Checkin.js
+++ src/scenes/Checkin.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  ScrollView,
   View,
+  Text,
   Image
 } from 'react-native';
+import CheckinThumb from '../components/CheckinThumb';
 
 export default class Checkin extends Component {
 
@@ -17,13 +20,31 @@ export default class Checkin extends Component {
     ),
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+
+  componentDidMount(){
+    var self = this;
+
+    fetch('http://192.168.1.40:8080/showlocations')
+    .then((response) => response.json())
+    .then((responseJson) => self.setState({data: responseJson}));
+  }
+
   render() {
     return (
       <View>
+        <ScrollView>
+          {this.state.data.map(createThumbRow)}
+        </ScrollView>
       </View>
     );
   }
 }
+
+var createThumbRow = (checkin, i) => <CheckinThumb key={i} source={checkin} />;
 
 var styles = StyleSheet.create({
   icon: {
